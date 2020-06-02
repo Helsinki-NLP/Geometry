0a35297c4da798327ab54a36522b25055971e6ad
jrvc <jraulvc@gmail.com>
2020-06-02 14:34:24 +0300

    changes

--- code/utils/logger.py
+++ code/utils/logger.py
@@ -1,8 +1,10 @@
 
 from datetime import datetime 
+import sys
 
 class logger():
     
     def info(string):
-        print(' | ',datetime.now().replace(microsecond=0), '|   '+string)
+        print(' | ',datetime.now().replace(microsecond=0), '|   '+string, flush=True)
+        #sys.stdout.flush()
 

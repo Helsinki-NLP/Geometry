e41e948b62410e357944abdeaf5255f77cbe5be4
Hande Celikkanat <celikkan@puhti-login2.bullx>
2020-06-02 13:30:35 +0300

    minor

--- code/utils/plotting.py
+++ code/utils/plotting.py
@@ -9,86 +9,86 @@ import argparse
 def makeplot(metrics, outdir):
     '''
     metrics[modname] = {'selfsim':selfsim,
-    	   'selfsim_isotropic': selfsim - b1,
-    	   'intrasentsim': insentsim,
-    	   'intrasentsim_isotropic': insentsim - b2,
-    	   'mev': mev,
-    	   'mev_isotropic_V1': mev - b3,
-    	   'mev_isotropic_V2': mev - b3bis,
-    	    }
+           'selfsim_isotropic': selfsim - b1,
+           'intrasentsim': insentsim,
+           'intrasentsim_isotropic': insentsim - b2,
+           'mev': mev,
+           'mev_isotropic_V1': mev - b3,
+           'mev_isotropic_V2': mev - b3bis,
+            }
     '''
 
     for modname in metrics:
-    	logger.info(f'plotting metrics for model {modname}')
+        logger.info(f'plotting metrics for model {modname}')
 
-    	n_layers = metrics[modname]['selfsim'].shape[1]
+        n_layers = metrics[modname]['selfsim'].shape[1]
         layers = range(1,n_layers+1)
 
         fig = plt.figure()
 
-    	#selfsim
+        #selfsim
         plt.subplot(6, 1, 1)
-    	per_layer_average = np.mean(metrics[modname]['selfsim'], 0).reshape((n_layers,1))
-    	plt.plot(layers, per_layer_average, '-.')
-    	plt.title(f'{modname} Word Self-Sim [Uncorrected]')
-    	plt.ylim([0, 1])
-    	plt.xticks(layers)
-    	plt.grid(True)
+        per_layer_average = np.mean(metrics[modname]['selfsim'], 0).reshape((n_layers,1))
+        plt.plot(layers, per_layer_average, '-.')
+        plt.title(f'{modname} Word Self-Sim [Uncorrected]')
+        plt.ylim([0, 1])
+        plt.xticks(layers)
+        plt.grid(True)
 
-    	#selfsim_isotropic
+        #selfsim_isotropic
         plt.subplot(6, 1, 2)
-    	per_layer_average = np.mean(metrics[modname]['selfsim_isotropic'], 0).reshape((n_layers,1))
-    	plt.plot(layers, per_layer_average, '-.')
-    	plt.title(f'{modname} Word Self-Sim [Isotropic]')
-    	plt.ylim([0, 1])
-    	plt.xticks(layers)
-    	plt.grid(True)
+        per_layer_average = np.mean(metrics[modname]['selfsim_isotropic'], 0).reshape((n_layers,1))
+        plt.plot(layers, per_layer_average, '-.')
+        plt.title(f'{modname} Word Self-Sim [Isotropic]')
+        plt.ylim([0, 1])
+        plt.xticks(layers)
+        plt.grid(True)
 
-    	#intrasentsim
+        #intrasentsim
         plt.subplot(6, 1, 3)
-    	per_layer_average = np.mean(metrics[modname]['intrasentsim'], 0).reshape((n_layers,1))
-    	plt.plot(layers, per_layer_average, '-.')
-    	plt.title(f'{modname} Intra-Sent Sim [Uncorrected]')
-    	plt.ylim([0, 1])
-    	plt.xticks(layers)
-    	plt.grid(True)
+        per_layer_average = np.mean(metrics[modname]['intrasentsim'], 0).reshape((n_layers,1))
+        plt.plot(layers, per_layer_average, '-.')
+        plt.title(f'{modname} Intra-Sent Sim [Uncorrected]')
+        plt.ylim([0, 1])
+        plt.xticks(layers)
+        plt.grid(True)
 
-    	#intrasentsim_isotropic
+        #intrasentsim_isotropic
         plt.subplot(6, 1, 4)
-    	per_layer_average = np.mean(metrics[modname]['intrasentsim_isotropic'], 0).reshape((n_layers,1))
-    	plt.plot(layers, per_layer_average, '-.')
-    	plt.title(f'{modname} Intra-Sent Sim [Isotopic]')
-    	plt.ylim([0, 1])
-    	plt.xticks(layers)
-    	plt.grid(True)
+        per_layer_average = np.mean(metrics[modname]['intrasentsim_isotropic'], 0).reshape((n_layers,1))
+        plt.plot(layers, per_layer_average, '-.')
+        plt.title(f'{modname} Intra-Sent Sim [Isotopic]')
+        plt.ylim([0, 1])
+        plt.xticks(layers)
+        plt.grid(True)
 
-    	#mev
+        #mev
         plt.subplot(6, 1, 5)
-    	per_layer_average = np.mean(metrics[modname]['mev'], 0).reshape((n_layers,1))
-    	plt.plot(layers, per_layer_average, '-.')
-    	plt.title(f'{modname} Max Explainable Var [Uncorrected]')
-    	plt.ylim([0, 1])
-    	plt.xticks(layers)
-    	plt.grid(True)
+        per_layer_average = np.mean(metrics[modname]['mev'], 0).reshape((n_layers,1))
+        plt.plot(layers, per_layer_average, '-.')
+        plt.title(f'{modname} Max Explainable Var [Uncorrected]')
+        plt.ylim([0, 1])
+        plt.xticks(layers)
+        plt.grid(True)
 
-    	#mev_isotropic_V1
-    	#per_layer_average = np.mean(metrics[modname]['mev_isotropic_V1'], 0).reshape((n_layers,1))
-    	#fig = plt.plot(layers, per_layer_average, '-.')
-    	#plt.title(f'{modname} Max Explainable Var [Isotropic V1]')
-    	#plt.ylim([0, 1])
-    	#plt.xticks(layers)
-    	#plt.grid(True)
-    	#plt.savefig(f'{outdir}/{modname}.mev_v1.png')
+        #mev_isotropic_V1
+        #per_layer_average = np.mean(metrics[modname]['mev_isotropic_V1'], 0).reshape((n_layers,1))
+        #fig = plt.plot(layers, per_layer_average, '-.')
+        #plt.title(f'{modname} Max Explainable Var [Isotropic V1]')
+        #plt.ylim([0, 1])
+        #plt.xticks(layers)
+        #plt.grid(True)
+        #plt.savefig(f'{outdir}/{modname}.mev_v1.png')
 
-    	#mev_isotropic_V2
+        #mev_isotropic_V2
         plt.subplot(6, 1, 6)
-    	per_layer_average = np.mean(metrics[modname]['mev_isotropic_V2'], 0).reshape((n_layers,1))
-    	plt.plot(layers, per_layer_average, '-.')
-    	plt.title(f'{modname} Max Explainable Var [Isotropic]')
-    	plt.ylim([0, 1])
-    	plt.xticks(layers)
-    	plt.grid(True)
-    	
+        per_layer_average = np.mean(metrics[modname]['mev_isotropic_V2'], 0).reshape((n_layers,1))
+        plt.plot(layers, per_layer_average, '-.')
+        plt.title(f'{modname} Max Explainable Var [Isotropic]')
+        plt.ylim([0, 1])
+        plt.xticks(layers)
+        plt.grid(True)
+        
         plt.savefig(f'{outdir}/{modname}.metrics.png')
 
  
